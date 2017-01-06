879bdc46a1f9d00e67c3f2650ef2939c07b231ab
jplmaldonado <jmaldonado@psgtecnologiaaplicada.com.br>
2017-01-06 15:31:48 -0300

    primeiro commit
    estrutura do site

---
+++ cabecalho.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="css/stylepsg.css" type="text/css">
+</head>
+<body>
+	<div class="container-fluid bgpsg">
+		<header> 
+			<div class="banner">
+			</div>
+			<nav class="navbar navbar-default">
+				<div class="container-fluid navpsg">			
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Home</a>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li><a href="#">Institucional</a></li>
+							<li><a href="#">Serviços</a></li>
+							<li><a href="#">Produtos</a></li>
+							<li><a href="#">Clientes</a></li>
+							<li><a href="#">Contato</a></li>
+							<li><a href="#">Trabalhe Conosco</a></li>
+							<li><a href="#">Acesso Usuário</a></li>
+						</ul>					
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</nav>
+	</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include_once("cabecalho.php");
+?>
+
+
+<div style="background: green">
+<h1> Pagina 1 </h1>
+</div>
+
+
+
+<?php
+include_once("rodape.php");
+?>
