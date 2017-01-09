4bedcb8ebb51deb731f87b76fd24ceab00085bcb
Gustavo Candido de Oliveira <gustavo.gcdo@gmail.com>
2017-01-09 16:07:16 -0300

    Adicionado pastas modelo I e II

---
+++ modelo_I/clientes.php
@@ -0,0 +1,37 @@
+<?php
+include_once("cabecalho.php");
+?>
+<div class="container">
+    <div class="conteudo-principal">
+
+        <div class="container-clientes-certificacoes">
+            <div class="clientes">
+                <h3>Clientes</h3>
+                <ul>
+                    <li><img src="img/cliente-1.png"</li>
+                    <li><img src="img/cliente-2.png"</li>
+                    <li><img src="img/cliente-3.png"</li>
+                    <li><img src="img/cliente-4.png"</li>
+                    <li><img src="img/cliente-5.png"</li>
+                </ul>
+            </div>
+
+            <hr>
+
+            <div class="certificacoes">
+                <h3>Certificações</h3>
+                <ul>
+                    <li><img src="img/doc.png"</li>
+                    <li><img src="img/doc.png"</li>
+                    <li><img src="img/doc.png"</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<?php
+include_once("rodape.php");
+?>
---
+++ modelo_II/rodape.php
@@ -0,0 +1,5 @@
+</div>
+</body>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</html>
