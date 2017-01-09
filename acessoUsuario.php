e62697fff0ee39c3db11e3e9afdd123f28129686
Gustavo Candido de Oliveira <gustavo.gcdo@gmail.com>
2017-01-09 11:34:12 -0300

    Ajustes Acesso Usuario

--- acessoUsuario.php
+++ acessoUsuario.php
@@ -3,11 +3,19 @@ include_once("cabecalho.php");
 ?>
 
 <div class="container">
-	<div class="boxAcessoUsuario"/>
-	<h2><img src="img/padlock.png"/> Acesso Usuário</h2><br/><br/><br/>
-	<h4><a href="#"><img src="img/holerite.png"/> Holerite<br/></a></h4>
-	<h4><a href="#"><img src="img/sgda.png"/> SGDA<br/></a></h4>
-	</div>
+    <div class="boxAcessoUsuario">
+        <h2><img src="img/padlock.png"/> Acesso Usuário</h2>
+
+        <div class="row">
+            <div class="col-sm-6 col-md-6">
+                <a href="#"><img src="img/holerite.png" />
+                    <h4>Holerite</h4></a>
+            </div>
+            <div class="col-sm-6 col-md-6">
+                <a href="#"><img src="img/sgda.png" /> <h4>SGDA</h4></a>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
