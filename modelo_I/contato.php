34fcfc88482ae84f0e44341694cd178fe4e5bb11
Gustavo Candido de Oliveira <gustavo.gcdo@gmail.com>
2017-01-09 23:02:33 -0300

    Tela Contato Atualização
    Adicionado OpenStreetMap

--- modelo_I/contato.php
+++ modelo_I/contato.php
@@ -2,40 +2,44 @@
 include_once("cabecalho.php");
 ?>
 <div class="container">
-<h1>Contato</h1>
- <div class="formulario">
-	<form class="form-horizontal">
-		<div class="form-group">
-		<label class="control-label col-sm-2" for="email">Nome:</label>
-			<div class="col-sm-10">
-				<input type="email" class="form-control" id="email" placeholder="Enter email">
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="pwd">Email:</label>
-			<div class="col-sm-10">          
-				<input type="password" class="form-control" id="pwd" placeholder="Enter password">
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="pwd">Telefone:</label>
-			<div class="col-sm-10">          
-				<input type="password" class="form-control" id="pwd" placeholder="Enter password">
-			</div>
-		</div>
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="pwd">Descrição:</label>
-			<div class="col-sm-10">          
-				<textarea class="form-control" rows="8" cols="60"></textarea>
-			</div>
-		</div>
-		<div class="form-group">        
-			<div class="col-sm-offset-2 col-sm-10">
-				<button type="submit" class="btn btn-success">Enviar</button>
-			</div>
-		</div>
-	</form>
- 	</div>
+    <h1>Contato</h1>
+    <form class="form-horizontal contato">
+        <div class="row">        
+            <div class="col-md-6">
+                <div class="formulario">
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="email">Nome:</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="email" placeholder="Enter email">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="pwd">Email:</label>
+                        <div class="col-sm-10">          
+                            <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="pwd">Telefone:</label>
+                        <div class="col-sm-10">          
+                            <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="pwd">Descrição:</label>
+                        <div class="col-sm-10">          
+                            <textarea class="form-control" rows="8" cols="60"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">                
+                <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=-54.641361236572266%2C-20.563688790851735%2C-54.574413299560554%2C-20.427012814257385&amp;layer=mapnik" style="border: 1px solid black"></iframe>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-success">Enviar</button>
+    </form>
 </div>
 
 <?php
